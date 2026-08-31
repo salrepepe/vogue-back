6c3cb01cb806b15b6bcd46b0a1f980e1f57594a1
salrepepe <muhadeputat@gmail.com>
2026-08-31 16:51:09 +0600

    a

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://www.vogueoriginals.com/"],
+    origin: ["http://localhost:5173", "https://vogueoriginals.com"],
   }),
 );
 
